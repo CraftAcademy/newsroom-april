00963b957693ef0b931f3dc5983887d75db6568f
nader <alghazu@gmail.com>
2018-05-23 15:57:26 +0200

    configure FactoryBot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,17 +7,15 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.use_transactional_fixtures = true
-
   config.infer_spec_type_from_file_location!
-
   config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
-    with.library :rails 
+    with.library :rails
   end
 end
