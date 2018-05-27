ea1dad68f34cd73cb3d9f58a4eb3c3eb5afe14d5
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2018-05-27 23:26:59 +0200

    Add role attribute to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
  describe 'Database table' do
    it { is_expected.to have_db_column :email }
    it { is_expected.to have_db_column :encrypted_password }
+   it { is_expected.to have_db_column :role }
  end
 
  describe 'Factory' do
