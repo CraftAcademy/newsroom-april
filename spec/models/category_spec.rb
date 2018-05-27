b915c349e239ecf920592d23ffa451d2a4842369
nader <alghazu@gmail.com>
2018-05-27 16:20:03 +0200

    address comments on pr: modify Rspec tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  let(:category) { create(:category) }
-
   describe 'Database table' do
     it { is_expected.to have_db_column :name }
   end
 
+  describe 'Association' do
+    it { is_expected.to have_many :articles }
+  end
+
   describe 'Validation' do
     it { is_expected.to validate_presence_of :name }
-    it { is_expected.to have_many :articles }
   end
 
   describe 'Factory' do
     it 'can create a valid instance' do
-      expect(category).to be_valid
+      expect(create(:category)).to be_valid
     end
   end
 end
