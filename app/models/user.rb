99bf478372a5ef896db5e9b55512b2a7691c8bc4
petra_bodman <petra_bodman@hotmail.com>
2018-05-31 10:38:44 +0200

    adds default user role as subscriber

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
+  after_initialize :set_default_role, if: :new_record?
+
   enum role: [:subscriber, :journalist, :editor]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   validates :role, presence: true
   has_many :articles
   has_many :comments
+
+  private
+
+    def set_default_role
+      self.role ||= :subscriber
+    end
 end
